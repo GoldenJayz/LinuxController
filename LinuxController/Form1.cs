e15f23514e7e6477eee4c11372a1d51c633718e9
GoldenJayz <jmdaniels150@gmail.com>
2022-01-02 10:21:22 -0500

    Added Execution for File Path

--- LinuxController/Form1.cs
+++ LinuxController/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace LinuxController
 {
@@ -20,21 +14,24 @@ namespace LinuxController
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             try
             {
                 openFileDialog1.ShowDialog();
 
                 string fileContent;
                 Stream fileStream;
+                string filePath;
 
                 fileStream = openFileDialog1.OpenFile();
+                filePath = openFileDialog1.FileName;
+                Console.WriteLine(filePath);
 
                 using (StreamReader reader = new StreamReader(fileStream))
                 {
                     fileContent = reader.ReadToEnd(); // Gets the data from the file and stores into fileContent
                 }
                 Console.WriteLine(fileContent);
+                Process.Start(@"cmd.exe", String.Format(@"/k {0}", filePath)); // run file
             }
             catch (FileNotFoundException) { Console.WriteLine("Invalid File!");  }
         }
