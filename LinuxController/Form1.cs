56c6a9f6127ef705cef98262631be9482b760007
GoldenJayz <jmdaniels150@gmail.com>
2022-01-02 09:40:27 -0500

    Getting the SSH Key File

--- LinuxController/Form1.cs
+++ LinuxController/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,27 @@ namespace LinuxController
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                openFileDialog1.ShowDialog();
+
+                string fileContent;
+                Stream fileStream;
+
+                fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd(); // Gets the data from the file and stores into fileContent
+                }
+                Console.WriteLine(fileContent);
+            }
+            catch (FileNotFoundException) { Console.WriteLine("Invalid File!");  }
         }
     }
 }
